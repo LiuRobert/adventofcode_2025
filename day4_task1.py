3519891cbbcbd2d442f1f50ea32d2a1338714dfb
LiuRobert <robert.ho89@gmail.com>
2025-12-04 09:29:32 +0100

    Day 4

---
+++ day4_task1.py
@@ -0,0 +1,27 @@
+import util
+
+def inbounds(lines, y, x):
+    return x >= 0 and y >= 0 and x < len(lines[0]) and y < len(lines)
+
+def is_roll(lines, y, x):
+    if inbounds(lines, y, x) and lines[y][x] == "@":
+        return 1
+    return 0
+
+lines = util.read_lines(False)
+res = 0
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if lines[y][x] == "@":
+            rolls = is_roll(lines, y - 1, x - 1)
+            rolls += is_roll(lines, y - 1, x)
+            rolls += is_roll(lines, y - 1, x + 1)
+            rolls += is_roll(lines, y, x - 1)
+            rolls += is_roll(lines, y, x + 1)
+            rolls += is_roll(lines, y + 1, x - 1)
+            rolls += is_roll(lines, y + 1, x)
+            rolls += is_roll(lines, y + 1, x + 1)
+            if rolls < 4:
+                res += 1
+
+print(res)
